7c2d37e2641e2bea0f90cae7ed0ba9af089efb21
jaferalilpu <104067033+jaferalilpu@users.noreply.github.com>
2025-02-08 17:03:27 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Tilt from 'react-parallax-tilt';
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Who Am I?</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       I am an aspiring full stack developer with a strong command of frameworks like React.js and Node.js.
+      I also hold expertise in databases such as SQL and NoSQL (like MongoDB). 
+      A part from these technologies, I am passionate about DevOps, where I possess strong knowledge of tools and 
+      practices including Git, GitHub, Jenkins, Ansible, Docker, Maven, and AWS. 
+      I have earned the AWS Cloud Essentials Certificate from Amazon. Additionally, 
+      I have a solid foundation in Operating Systems (OS), Data Structures and Algorithms (DSA), Software Engineering, Project Management, 
+      and Networking.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,213 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  nodejs,
+  mongodb,
+  git,
+  docker,
+  programmingPathshala,
+  physicsWallah,
+  carrent,
+  jobit,
+  tripguide,
+} from "../assets";
+
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Software Engineer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Devops Engineer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Frontend Web Developer Trainee",
+    company_name: "Programming Pathshala",
+    icon: programmingPathshala, 
+    iconBg: "#383E56",
+    date: "April 2023 - August 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Working on Real time applications and building Realtime applications",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Trainee Engineer",
+    company_name: "Physics Wallah",
+    icon: physicsWallah,
+    iconBg: "#E6DEDD",
+    date: "December 2024 - Present",
+    points: [
+      "Acquiring hands-on experience in developing, implementing, and maintaining internalweb applications.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Enhancing the technologies with integration of DevOps",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website, but Jafer proved me wrong.",
+    name: "Shamreena",
+    designation: "Associate Software Engineer",
+    company: "LTTS",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've met a web developer who truly loves development' like Jafer Ali.",
+    name: "Naveen Arun Jaswanth",
+    designation: "SDE",
+    company: "Accenture",
+    image: "https://media.licdn.com/dms/image/v2/D5635AQFlDkYZeNTJFA/profile-framedphoto-shrink_100_100/profile-framedphoto-shrink_100_100/0/1706512585877?e=1739617200&v=beta&t=cDlqEafjsT616aAILpVzWCxexzSID0EC7XFqVgdJlmk",
+  },
+  {
+    testimonial:
+      "After Jafer Joined with me, We are now ready to bring new change in Development",
+    name: "Saniya",
+    designation: "Junior Software Engineer",
+    company: "Microsoft",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "PHP",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "NodeJS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/jaferalilpu",
+  },
+  {
+    name: "Health Application",
+    description:
+      "Web application that enables users to Book appointments with doctor, view patients who booked appointments, and for patients it allows to check appointments with doctors.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Node JS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Mongo DB",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/jaferalilpu",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/jaferalilpu",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
